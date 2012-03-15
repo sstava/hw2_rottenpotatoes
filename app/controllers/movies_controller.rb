d3e67e0e151bd707ba563ca692fab91e4ff6df07
Steve Stava <steve.stava@gmail.com>
2012-03-14 20:09:13 -0500

    all the clean up coding to complete the assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,37 +7,52 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @rating_checked =
-    if !params[:ratings].nil?
-      @selected_ratings = []
+    @rating_checked = {}                      # this is used to re-set the check boxes
+    @selected_ratings = []                    # this is used to pass the ratings array to the db query
+
+    if !params[:ratings].nil?                 # only do this if there ARE selected ratings
       @ratings = params[:ratings]
       @ratings.each do |key, value|
         @selected_ratings << key
+        @rating_checked[key] = true
       end
-      @query_ratings = @selected_ratings
-    elsif !session[:ratings].nil?
-      @selected_ratings = []
-      @ratings = session[:ratings]
+      session[:ratings] = @selected_ratings
+    elsif params[:commit] == 'Refresh'        # do this if there aren't selected ratings, but Refresh param was there
+      Movie.all_ratings.each do |r|           # - this would mean that someone unselected all checkboxes but did a
+        @rating_checked[r] = true             # - Refresh of selected movies based on ratings
+      end
+      @selected_ratings = Movie.all_ratings
+      session[:ratings] = @selected_ratings
+    elsif !session[:ratings].nil?             # you have come from a different page and need to reset the ratings from
+      @ratings = session[:ratings]            # - a prior selection
       @ratings.each do |key, value|
         @selected_ratings << key
+        @rating_checked[key] = true
       end
-      @query_ratings = @selected_ratings
     else
-      @query_ratings = Movie.all_ratings
+      Movie.all_ratings.each do |r|           # no refresh button, no prior session, probably the first time in to the
+        @rating_checked[r] = true             # - page
+      end
+      @selected_ratings = Movie.all_ratings
     end
 
     if params[:sort_title] == 'hilite'
-      @movies = Movie.where(:rating => @query_ratings, :order => "title ASC")
-      #@movies = Movie.all(:order => "title ASC")
+      @movies = Movie.find_all_by_rating(@selected_ratings).sort_by { |m| m.title }
+      session[:sort_title] = 'hilite'
+      session.delete(:sort_date)
     elsif params[:sort_date] == 'hilite'
-      @movies = Movie.where(:rating => @query_ratings, :order => "release_date ASC")
-      #@movies = Movie.all(:order => "release_date ASC")
+      @movies = Movie.find_all_by_rating(@selected_ratings).sort_by { |m| m.release_date }
+      session[:sort_date] = 'hilite'
+      session.delete(:sort_title)
+    elsif session[:sort_title] == 'hilite'
+      @movies = Movie.find_all_by_rating(@selected_ratings).sort_by { |m| m.title }
+    elsif session[:sort_date] == 'hilite'
+      @movies = Movie.find_all_by_rating(@selected_ratings).sort_by { |m| m.release_date }
     else
-      @movies = Movie.where(:rating => @query_ratings)
-      #@movies = Movie.all
+      @movies = Movie.where(:rating => @selected_ratings)
     end
 
-    @all_ratings = Movie.all_ratings
+    @all_ratings = Movie.all_ratings           # setup the @all_ratings variable for the view
 
   end
 
